98ccfc66f0986cdbbc20bc1177b65da05f6d5560
luluzhangg <zhanl910@newschool.edu>
2023-05-03 23:56:48 -0400

    main

--- core2/studio/nyc-data/archive/assets/script.js
+++ core2/studio/nyc-data/archive/assets/script.js
@@ -58,6 +58,72 @@ function renderItems(collection) {
 	})
 }
 
+let femaleFilterBtn = document.getElementById("female");
+femaleFilterBtn.addEventListener("click", function (event) {
+  console.log("female button", event.target.id);
+  let listofItems = document.querySelectorAll(".causes");
+
+  listofItems.forEach(function searchCardioFilter(item) {
+    if (item.querySelector("p").innerHTML === "female") {
+      item.classList.add("filter-show");
+      item.classList.remove("filter-hide");
+      console.log(item);
+    } else {
+      item.classList.add("filter-hide");
+      item.classList.remove("filter-show");
+    }
+  });
+});
+
+// let maleilterBtn = document.getElementById("male");
+// maleFilterBtn.addEventListener("click", function (event) {
+//   console.log("male button", event.target.id);
+//   let listofItems = document.querySelectorAll(".causes");
+
+//   listofItems.forEach(function searchCardioFilter(item) {
+//     if (item.querySelector("p").innerHTML === "female") {
+//       item.classList.add("filter-show");
+//       item.classList.remove("filter-hide");
+//       console.log(item);
+//     } else {
+//       item.classList.add("filter-hide");
+//       item.classList.remove("filter-show");
+//     }
+//   });
+// });
+
+let sortToggleBtn = document.getElementById("sort-toggle");
+let isSortingAlphabetically = false;
+
+sortToggleBtn.addEventListener("click", function (event) {
+  isSortingAlphabetically = !isSortingAlphabetically;
+  sortAirtableItems(isSortingAlphabetically);
+});
+
+function sortAirtableItems(isSortingAlphabetically) {
+  let container = document.getElementById("dataset");
+  let items = Array.from(document.querySelectorAll(".causes"));
+
+  if (isSortingAlphabetically) {
+    items.sort((a, b) => {
+      let nameA = a.querySelector("p").innerHTML.toLowerCase();
+      let nameB = b.querySelector("p").innerHTML.toLowerCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  } else {
+    items.sort((a, b) => a.dataset.index - b.dataset.index);
+  }
+
+  items.forEach((item) => container.appendChild(item));
+}
+
+
 fetch('assets/data.json')
 	.then(function(response) {
 		return response.json();	
