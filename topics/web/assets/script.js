edf84f6435bde831e2923cb284b94fac4d94c18d
luluzhangg <zhanl910@newschool.edu>
2023-10-05 00:39:23 -0400

    main

---
+++ topics/web/assets/script.js
@@ -0,0 +1,22 @@
+var divs = document.getElementsByTagName('img');
+var winWidth = window.innerWidth;
+var winHeight = window.innerHeight;
+
+for ( var i=0; i < divs.length; i++ ) {
+ 	
+    var thisDiv = divs[i];
+    
+    randomTop = getRandomNumber(0, winHeight);
+    randomLeft = getRandomNumber(0, winWidth);
+    
+    // update top and left position
+    thisDiv.style.top = randomTop +"px";
+    thisDiv.style.left = randomLeft +"px";
+    
+}
+
+function getRandomNumber(min, max) {
+    
+  return Math.random() * (max - min) + min;
+    
+}
